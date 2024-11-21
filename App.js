2cc691b86c558ac90839a6f0b417db51ba1a3796
FadlyHytam <fadlymaulanaiksan@student.uir.ac.id>
2024-11-21 10:57:34 +0700

    first commit

--- App.js
+++ App.js
@@ -4,7 +4,15 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* Teks di tengah layar */}
+      <Text style={styles.text}>gacorrr</Text>
+
+      {/* Kotak dengan tata letak FlexBox */}
+      <View style={styles.boxContainer}>
+        <View style={styles.box1} />
+        <View style={styles.box2} />
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +21,30 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#D3D3D3', 
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    fontSize: 24, 
+    color: 'blue', 
+    fontWeight: 'bold', 
+    marginBottom: 20, 
+  },
+  boxContainer: {
+    flexDirection: 'row', 
+    justifyContent: 'space-between',
+    width: '80%', 
+    marginTop: 20, 
+  },
+  box1: {
+    width: 100, 
+    height: 100, 
+    backgroundColor: 'black', 
+  },
+  box2: {
+    width: 100, 
+    height: 100, 
+    backgroundColor: 'brown', 
   },
 });
